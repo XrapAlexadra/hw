04deab8d2244c5591c9d10dac2ef35dd062d2171
alexxrap <xrapalexandra@gmail.com>
2020-03-30 15:59:35 +0300

    NothingToDo

--- service/src/main/java/com/gmail/xrapalexandra/service/impl/UserServiceImpl.java
+++ service/src/main/java/com/gmail/xrapalexandra/service/impl/UserServiceImpl.java
@@ -56,4 +56,8 @@ public class UserServiceImpl implements UserService {
     public User delUserFromRepository(String login) {
         return authUser.delUserByLogin(login);
     }
+
+    private void NothingToDo(){
+
+    }
 }
